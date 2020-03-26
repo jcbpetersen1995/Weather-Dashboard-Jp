37f02468d0c92a72411004e4fe7fdf227c084310
Jacob Petersen <jcb.petersen1995@gmail.com>
2020-03-26 12:04:13 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+
 $(document).ready(function () {
     console.log("ready!");
     ///////////////////////////////////////////////////////
     $("#searchButton").on("click", function () {
         event.preventDefault();
         var citySearch = $("#citySearch").val().trim();
-        const APIKey = "adadf6b95c603c845d08323eaa0bc6ea"
-        const query1 = "api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIKey;
+        const APIKey = "536846aa4d5ff654f203c2a1d2b6526a"
+        const query1 = "http://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIKey;
+       // const query2 = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat={lat}&lon={lon}"
         console.log(citySearch);
         console.log(query1);
         //////ajax call
@@ -13,10 +15,15 @@ $(document).ready(function () {
             url: query1,
             method: "GET"
         })
-            .then(function(response) {
-                $("#cityTitle").text(response.temp)
-            }
-            )
+            .then(function (response) {
+                console.log(response)
+                var temperature = (((response.main.temp - 273.15)*1.8)+32).toFixed();
+                $("#cityTitle").text(response.name);
+                $("#temperature").text("Temperature: " + temperature + " Degrees");
+                $("#humidity").text("Humidity: " + response.main.humidity + "%");
+                $("#wind").text("Wind Speed: " + response.wind.speed + " MPH");
+                //$("uv").text("UV Index: " + response.city.coord.lat.toFixed(2));
+            });
     });
-
+//////////////////////
 });
