c6bf8d8d4a0904f393b3159b750d848c64e23a1c
Jacob Petersen <jcb.petersen1995@gmail.com>
2020-03-26 16:43:06 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
+var today = moment().format('MMMM Do YYYY');
 
 $(document).ready(function () {
     console.log("ready!");
     ///////////////////////////////////////////////////////
-    $("#searchButton").on("click", function () {
+    $("#searchButton").on("click",function() {
         event.preventDefault();
         var citySearch = $("#citySearch").val().trim();
         const APIKey = "536846aa4d5ff654f203c2a1d2b6526a"
         const query1 = "http://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=" + APIKey;
-       // const query2 = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat={lat}&lon={lon}"
         console.log(citySearch);
         console.log(query1);
         //////ajax call
@@ -17,13 +17,39 @@ $(document).ready(function () {
         })
             .then(function (response) {
                 console.log(response)
-                var temperature = (((response.main.temp - 273.15)*1.8)+32).toFixed();
-                $("#cityTitle").text(response.name);
+                var temperature = (((response.main.temp - 273.15) * 1.8) + 32).toFixed();
+                $("#cityTitle").text(response.name + " - " + today);
                 $("#temperature").text("Temperature: " + temperature + " Degrees");
                 $("#humidity").text("Humidity: " + response.main.humidity + "%");
                 $("#wind").text("Wind Speed: " + response.wind.speed + " MPH");
-                //$("uv").text("UV Index: " + response.city.coord.lat.toFixed(2));
+                var lon = response.coord.lon;
+                var lat = response.coord.lat;
+                const query2 = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&lat=" + lat + "&lon=" + lon;
+                $.ajax({
+                    url: query2,
+                    method: "GET"
+                }).then(function (response) {
+                    var uvIndex = $("#uv");
+                    uvIndex.text("UV Index: " + response.value);
+                    if (response.value <= 2.99) {
+                        uvIndex.attr("class", "btn btn-success");
+                    };
+                    if (response.value >= 3 && response.value <= 5.99) {
+                        uvIndex.attr("class", "btn btn-primary");
+                    };
+                    if (response.value >= 6 && response.value <= 7.99) {
+                        uvIndex.attr("class", "btn btn-warning");
+                    };
+                    if (response.value >= 8 && response.value <= 10.99) {
+                        uvIndex.attr("class", "btn btn-danger");
+                    };
+                    if (response.value > 11) {
+                        uvIndex.attr("class", "btn btn-dark")
+                    }
+                })
             });
     });
-//////////////////////
+    
+
+    //////////////////////
 });
